7c04dcbc0a756d365566c9dd39619d9d1feafd5d
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2023-08-23 11:19:50 +1000

    Added createdAt field

--- src/app/components/post-card.tsx
+++ src/app/components/post-card.tsx
@@ -1,19 +1,28 @@
-'use client'
+"use client";
 
-import { Card, CardHeader, CardBody, Avatar, CardFooter } from '@nextui-org/react'
-import Link from 'next/link'
-import { IconHeart, IconMessageCircle, IconRepeat } from '@tabler/icons-react'
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Avatar,
+  CardFooter,
+} from "@nextui-org/react";
+import Link from "next/link";
+import { IconHeart, IconMessageCircle, IconRepeat } from "@tabler/icons-react";
+import { getFormattedDate } from "../utils";
 
-export default function PostCard ({
+export default function PostCard({
   userFullName,
   userName,
   avatarUrl,
-  content
+  content,
+  createdAt,
 }: {
-  userFullName: string
-  userName: string
-  avatarUrl: string
-  content: string
+  userFullName: string;
+  userName: string;
+  avatarUrl: string;
+  content: string;
+  createdAt: string;
 }) {
   return (
     <Card className="shadow-none bg-transparent hover:bg-slate-800 transition border-b rounded-none cursor-pointer border-white/20">
@@ -23,28 +32,29 @@ export default function PostCard ({
             <Avatar radius="full" size="md" src={avatarUrl} />
           </Link>
           <div className="flex flex-col gap-1 items-start justify-center">
-            <h4 className="text-small font-semibold leading-none text-default-600">{userFullName}</h4>
-            <h5 className="text-small tracking-tight text-default-400">@{userName}</h5>
+            <h4 className="text-small font-semibold leading-none text-default-600">
+              {userFullName}
+            </h4>
+            <h5 className="text-small tracking-tight text-default-400">
+              @{userName} · {getFormattedDate(new Date(createdAt))}
+            </h5>
           </div>
         </div>
-
       </CardHeader>
       <CardBody className="px-3 py-0 text-xs text-white bg-transparent">
-        <p>
-          {content}
-        </p>
+        <p>{content}</p>
       </CardBody>
       <CardFooter className="gap-3">
         <button>
-          <IconMessageCircle className='w-4 h-4' />
+          <IconMessageCircle className="w-4 h-4" />
         </button>
         <button>
-          <IconHeart className='w-4 h-4' />
+          <IconHeart className="w-4 h-4" />
         </button>
         <button>
-          <IconRepeat className='w-4 h-4' />
+          <IconRepeat className="w-4 h-4" />
         </button>
       </CardFooter>
     </Card>
-  )
+  );
 }
