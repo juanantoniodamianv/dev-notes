8de1031cf66febcd61bc4da2a63741e5888de5a2
Juan Antonio Damian Vargas <juanantoniodamianv@gmail.com>
2023-08-23 11:19:04 +1000

    Added Navbar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { AuthButtonServer } from "./components/auth-button-server";
 import { PostLists } from "./components/posts-list";
 import { ComposePost } from "./components/compose-post";
+import Navbar from "./components/navbar";
 
 export default async function Home() {
   const supabase = createServerComponentClient({ cookies });
@@ -23,6 +24,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
+      <Navbar userAvatarUrl={session.user?.user_metadata?.avatar_url} userName={session.user?.user_metadata?.user_name} userFullName={session.user?.user_metadata?.full_name}/>
       <section className="max-w-[600px] w-full mx-auto border-l border-r border-white/20 min-h-screen">
         <ComposePost userAvatarUrl={session.user?.user_metadata?.avatar_url} />
         <PostLists posts={posts} />
